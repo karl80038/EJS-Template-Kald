5f394bc478394c9d4e7a3e8f2d669368a1ebda2b
Opilane TTHK <opilane@tthk.ee>
2020-02-26 17:29:32 +0200

    Added prerequisite packages and files

---
+++ routes/admin.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const rootDirectory = require('../utilities/path');
+const router = express.Router();
+const products = [];
+
+
+router.get('/add-product', function(req, res){
+    //res.sendFile(path.join(rootDirectory, 'views', 'add-product.html'));
+    res.render('add-product', 
+    {
+        pageTitle: "Add Product",
+        products: "Shop",
+        addProduct: "Add Product",
+        itemTitle: "Title",
+        addProductBtn: "Add"
+
+    });
+});
+//Submit user-entered data from the form using POST method
+router.post('/add-product', function(req, res){
+    console.log(req.body.title);
+    products.push({title: req.body.title});
+    res.redirect('/');
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const path = require('path');
+//injecting code from shop.js
+const userRoute = require('./routes/shop');
+const adminRoute = require('./routes/admin');
+const app = express();
+
+//Use of the Path package ensures the server always finds the file regardless of the operating system or file system the app currently runs on.
+//path.join(__dirname, 'folder', 'file.html')
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+
+app.use(userRoute);
+app.use('/admin',adminRoute);
+
+
+app.use(function(req, res, next){
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    //console.log(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', 
+    {pageTitle: "Page not Found.", 
+    pageNotFound: "Our apologies, but we were unable to find the requested page.",
+    message: "Please view our latest offerings.",});
+
+})
+app.listen(5000, ()=> {
+    console.log("Server is running on port 3000.");
+})
